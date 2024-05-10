f9facb19b54bce8f0fce550a7882a008ac5ed115
Umar <umar@gmail.com>
2024-05-10 20:04:46 +0500

    init

---
+++ PdfMerge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Documents.Primitives;
+using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf.Streaming;
+
+namespace MergePdfWithTelerik
+{
+    public class PdfMerge
+    {
+
+        public void MergePdf(string FilePathOne, string FilePathTwo, string MergeFilePath) {
+            string firstDocument = FilePathOne;
+            string secondDocument = FilePathTwo;
+
+            string exportedPdf = MergeFilePath;
+            if (File.Exists(exportedPdf))
+            {
+                File.Delete(exportedPdf);
+            }
+
+            using (FileStream fileStream = File.OpenWrite(exportedPdf))
+            {
+                using (PdfStreamWriter fileWriter = new PdfStreamWriter(fileStream))
+                {
+                    using (PdfFileSource firstDocumentToMerge = new PdfFileSource(File.OpenRead(firstDocument)))
+                    using (PdfFileSource secondDocumentToMerge = new PdfFileSource(File.OpenRead(secondDocument)))
+                    {
+                        foreach (var page in firstDocumentToMerge.Pages)
+                        {
+                            fileWriter.WritePage(page);
+                        }
+                        foreach(var page in secondDocumentToMerge.Pages)
+                        {
+                            fileWriter.WritePage(page);
+                        }
+                        
+                    }
+                }
+            }
+
+        }
+
+       
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergePdfWithTelerik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string filePath1 = @"C:/Users/Muham/Downloads/TourConfirmation.pdf";
+            string filePath2 = @"C:/Users/Muham/Downloads/Terms & Conditions.pdf";
+            string outFilePath = @"C:/Users/Muham/Downloads/MergeFile.pdf";
+            PdfMerge pdfMerge = new PdfMerge(); 
+            pdfMerge.MergePdf(filePath1, filePath2 , outFilePath);
+            Console.WriteLine("Merged");
+            Console.ReadLine();
+        }
+    }
+}
